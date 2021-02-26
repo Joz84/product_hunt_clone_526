8453e779b9b7a40d7e4c570e455f9aac9bca3781
jonathan serafini <jonathan.serafini@gmail.com>
2021-02-26 10:30:45 +0100

    add travais

--- test/system/products_test.rb
+++ test/system/products_test.rb
@@ -7,4 +7,29 @@ class ProductsTest < ApplicationSystemTestCase
     assert_selector "h1", text: "Ma super liste de produits"
     assert_selector ".card-product", count: Product.count
   end
+
+    # Me logguer
+    # aller sur la page new
+    # replir les champs de formulaire
+    # cliquer sur le bouton create
+    # arriver sur la page index
+    # Retrouver le produit sur la page index
+
+  test "lets a signed in user create a new product" do
+    login_as users(:george)
+    visit "/products/new"
+    # save_and_open_screenshot
+
+    fill_in "product_name", with: "Le Wagon"
+    fill_in "product_tagline", with: "Change your life: Learn to code"
+    # save_and_open_screenshot
+
+    click_on 'Create Product'
+    # save_and_open_screenshot
+
+    # Should be redirected to Home with new product
+    assert_equal root_path, page.current_path
+    assert_text "Change your life: Learn to code"
+  end
+
 end
